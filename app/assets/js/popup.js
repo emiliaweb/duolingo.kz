0a422ad513f10b84fb8d5242b8ef971c4e288ee0
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-04-28 17:48:52 +0600

    Prevent scroll

--- app/assets/js/popup.js
+++ app/assets/js/popup.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     const modal = document.querySelector('[data-popup]');
     const triggers = document.querySelectorAll('[data-trigger]');
+    const body = document.body;
 
     triggers.forEach(item => {
         item.addEventListener('click', e => {
             e.preventDefault();
             modal.classList.add('popup--active');
+            body.classList.add('no-scroll');
         });
     });
 
@@ -14,6 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if (target.matches('[data-overlay]')) {
             modal.classList.remove('popup--active');
+            body.classList.remove('no-scroll');
         }
     });
 });
